c1758f1ebca4a6f352ad58f9dfa2802b235a8f0b
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2023-10-23 09:47:07 +0200

    Adding candidates service.

---
+++ src/main/java/com/live/debate/candidates/CandidatesApplication.java
@@ -0,0 +1,13 @@
+package com.live.debate.candidates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/live/debate/candidates/CandidatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.live.debate.candidates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
